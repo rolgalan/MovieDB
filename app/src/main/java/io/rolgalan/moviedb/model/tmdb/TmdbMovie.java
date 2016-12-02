f58bbbe80233df60630b7a9b2e28e475ca62c093
rolgalan <rolgalan@gmail.com>
2016-12-02 17:12:42 +0100

    Fix TmdbMovie createFake() method

--- app/src/main/java/io/rolgalan/moviedb/model/tmdb/TmdbMovie.java
+++ app/src/main/java/io/rolgalan/moviedb/model/tmdb/TmdbMovie.java
@@ -20,7 +20,7 @@ public class TmdbMovie implements Movie {
     }
 
     public static TmdbMovie createFake(int i) {
-        return new TmdbMovie("Title " + i, "Year " + 1916 + Math.random() * 100, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "");
+        return new TmdbMovie("Title " + i, String.valueOf((int) (1916 + Math.random() * 100)), "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", "");
     }
 
     @Override
