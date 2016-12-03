8b43e28e9286bbe98f2f6503a7aef2ba437cfc86
rolgalan <rolgalan@gmail.com>
2016-12-03 14:15:13 +0100

    Handle configuration response

--- app/src/main/java/io/rolgalan/moviedb/data/DataProvider.java
+++ app/src/main/java/io/rolgalan/moviedb/data/DataProvider.java
@@ -3,9 +3,11 @@ package io.rolgalan.moviedb.data;
 import io.rolgalan.moviedb.model.LinkedMovieList;
 import io.rolgalan.moviedb.model.Movie;
 import io.rolgalan.moviedb.model.MovieList;
+import io.rolgalan.moviedb.model.tmdb.Configuration;
 import io.rolgalan.moviedb.model.tmdb.TmdbMovie;
 import io.rolgalan.moviedb.server.ApiManager;
 import io.rolgalan.moviedb.server.ServerResponseInterface;
+import io.rolgalan.moviedb.server.model.ConfigurationResponse;
 import io.rolgalan.moviedb.server.model.MovieServer;
 import io.rolgalan.moviedb.server.model.SearchResponse;
 
@@ -15,6 +17,7 @@ import io.rolgalan.moviedb.server.model.SearchResponse;
 
 public class DataProvider {
     public static final MovieList ITEMS = new LinkedMovieList();
+    private static Configuration configuration;
 
     public static void discoverMovies(DataInterface<MovieList> listener) {
         ApiManager.getInstance().discoverMovies(new SearchResponseBoundary(listener));
@@ -22,6 +25,22 @@ public class DataProvider {
 
     public static void searchMovies(String query, DataInterface<MovieList> listener) {
         ApiManager.getInstance().searchMovies(query, new SearchResponseBoundary(listener));
+    }
+
+    public static void getConfiguration(DataInterface<Configuration> listener) {
+        ApiManager.getInstance().getConfiguration(new ConfigurationResponseBoundary(listener));
+    }
+
+    public static Configuration getConfiguration() {
+        if (configuration == null) getConfiguration(null);
+        return configuration;
+    }
+
+    public static String composeImageUrl(String imagePath){
+        if (configuration!=null){
+            return configuration.composeImageUrl(imagePath);
+        }
+        return null;
     }
 
     private static class SearchResponseBoundary implements ServerResponseInterface<SearchResponse> {
@@ -42,13 +61,34 @@ public class DataProvider {
                 }
                 ITEMS.clear();
                 ITEMS.addAll(list);
-                listener.onResultsReceived(list);
+                if (listener != null) listener.onResultsReceived(list);
             }
         }
 
         @Override
         public void onError(String error) {
-            listener.onError(error);
+            if (listener != null) listener.onError(error);
+        }
+    }
+
+    private static class ConfigurationResponseBoundary implements ServerResponseInterface<ConfigurationResponse> {
+        final DataInterface<Configuration> listener;
+
+        public ConfigurationResponseBoundary(DataInterface<Configuration> listener) {
+            this.listener = listener;
+        }
+
+        @Override
+        public void onResultsReceived(ConfigurationResponse response) {
+            if (response != null) {
+                configuration = new Configuration(response.getBaseUrl(), response.getPosterSizes());
+                if (listener != null) listener.onResultsReceived(configuration);
+            }
+        }
+
+        @Override
+        public void onError(String error) {
+            if (listener != null) listener.onError(error);
         }
     }
 }
