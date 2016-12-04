fb0ae44b3f38b83db428c4e0e54c743e527ded87
rolgalan <rolgalan@gmail.com>
2016-12-04 20:13:54 +0100

    Add search bar and remove toolbar and menu

--- app/src/main/java/io/rolgalan/moviedb/MainActivity.java
+++ app/src/main/java/io/rolgalan/moviedb/MainActivity.java
@@ -2,11 +2,14 @@ package io.rolgalan.moviedb;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.rolgalan.moviedb.data.DataProvider;
+import io.rolgalan.moviedb.server.ApiManager;
 
 public class MainActivity extends AppCompatActivity {
     public final static String TAG = "MovieDB";
@@ -16,29 +19,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         DataProvider.getConfiguration();
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
