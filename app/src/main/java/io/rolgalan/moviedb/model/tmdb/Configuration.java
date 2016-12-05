88335c42207941089ab3698164bf4620048330de
rolgalan <rolgalan@gmail.com>
2016-12-05 17:21:00 +0100

    Improve configuration parsing for extract default image size

--- app/src/main/java/io/rolgalan/moviedb/model/tmdb/Configuration.java
+++ app/src/main/java/io/rolgalan/moviedb/model/tmdb/Configuration.java
@@ -7,10 +7,31 @@ package io.rolgalan.moviedb.model.tmdb;
 public class Configuration {
     private final String baseImagesUrl;
     private final String[] posterSizes;
+    private final String defaultSize;
 
     public Configuration(String baseImagesUrl, String[] posterSizes) {
         this.baseImagesUrl = baseImagesUrl;
         this.posterSizes = posterSizes;
+        defaultSize = extractDefaultSize(posterSizes);
+    }
+
+    /**
+     * Safely set a default size from the available poster sizes array.
+     * For a phone device we don't need a big image, but smalles one would be too small,
+     * so we choose the second smallest size (position 1 in the array).
+     * <p>
+     * However, we don't know how many sizes are available in the API, so this
+     * method could use the zero position if there is just one result.
+     *
+     * @param posterSizes
+     * @return
+     */
+    protected static String extractDefaultSize(String[] posterSizes) {
+        if (posterSizes != null && posterSizes.length > 0) {
+            if (posterSizes.length > 1) return posterSizes[1];
+            return posterSizes[0];
+        }
+        return "";
     }
 
     public String getBaseImagesUrl() {
@@ -22,6 +43,6 @@ public class Configuration {
     }
 
     public String composeImageUrl(String imgPath) {
-        return baseImagesUrl + posterSizes[1] + imgPath;
+        return baseImagesUrl + defaultSize + imgPath;
     }
 }
